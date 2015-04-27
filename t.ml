f28127d5b09bd5685aa4689b095984d4ad70a753
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-04-27 16:50:02 -0400

    Better statistics

--- t.ml
+++ t.ml
@@ -12,28 +12,74 @@ let time thunk =
   let delta = Unix.gettimeofday() -. start in
   (delta, result)
 
-let mean_of n scale label thunk =
-  let rec loop remaining acc result =
+let rec sum_of samples = match samples with
+  | [] -> 0.0
+  | x :: xs -> x +. sum_of xs
+
+let mean_of samples = sum_of samples /. (float_of_int (List.length samples))
+
+let sqr_sub mean x =
+  let x_minus_mean = x -. mean in
+  x_minus_mean *. x_minus_mean
+
+let unbiased_sample_variance_of samples =
+  let mean = mean_of samples in
+  sum_of (List.map (sqr_sub mean) samples) /. (float_of_int (List.length samples - 1))
+
+let sample_standard_deviation_of samples = sqrt (unbiased_sample_variance_of samples)
+
+let standard_error_of_mean_of samples =
+  sample_standard_deviation_of samples /. sqrt (float_of_int (List.length samples))
+
+let stats_header additional_info =
+  Printf.printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n%!"
+    "label"
+    "lo_95ci_microsec"
+    "mean_microsec"
+    "hi_95ci_microsec"
+    "lo_95ci_rate_kHz"
+    "mean_rate_kHz"
+    "hi_95ci_kHz"
+    "nrepeats"
+    "problem_size"
+    (String.concat "," additional_info)
+
+let stats n scale additional_info label thunk =
+  let rec loop remaining =
     if remaining > 0
     then
-      let (delta, result) = time thunk in
-      loop (remaining - 1) (acc +. delta) (Some result)
-    else (acc, result)
+      let (delta, _) = time thunk in
+      delta :: loop (remaining - 1)
+    else []
   in
-  let (total, result) = loop n 0.0 None in
-  let mean = total /. (float_of_int n) in
-  let scaled_mean = mean /. (float_of_int scale) in
-  Printf.printf "%s: %g µs/op (%g kHz; mean of %d runs)\n%!"
+  let result = thunk() in (* discard a single run to warm up *)
+  let samples = loop n in
+  let mean = mean_of samples in
+  let standard_error_of_mean = standard_error_of_mean_of samples in
+  let half_ci = 1.96 *. standard_error_of_mean in
+  let lo_ci = mean -. half_ci in
+  let hi_ci = mean +. half_ci in
+  let scale_f = float_of_int scale in
+  let scaled_mean = mean /. scale_f in
+  let scaled_lo_ci = lo_ci /. scale_f in
+  let scaled_hi_ci = hi_ci /. scale_f in
+  let microseconds = 1000000.0 in
+  let one_millisecond = 0.001 in
+  Printf.printf "%s,%g,%g,%g,%g,%g,%g,%d,%d,%s\n%!"
     label
-    (scaled_mean *. 1000000.0)
-    (0.001 /. scaled_mean)
-    n;
-  match result with None -> raise Not_found | Some v -> v
+    (scaled_lo_ci *. microseconds)
+    (scaled_mean *. microseconds)
+    (scaled_hi_ci *. microseconds)
+    (one_millisecond /. scaled_hi_ci)
+    (one_millisecond /. scaled_mean)
+    (one_millisecond /. scaled_lo_ci)
+    n
+    scale
+    (String.concat "," additional_info);
+  result
 
-let str4 i =
-  let padding = 10000 in
+let str4 padding base i =
   let s = String.make (padding + 4) 'x' in
-  let base = 9000 in
   String.set s (base + 0) (char_of_int (i lsr 24));
   String.set s (base + 1) (char_of_int ((i lsr 16) land 255));
   String.set s (base + 2) (char_of_int ((i lsr 8) land 255));
@@ -53,12 +99,23 @@ let main () =
   (* dump x'; *)
   (* dump x''; *)
 
-  let max_count = 50000 in
-  let nrepeats = 15 in
+  let (max_count, nrepeats, padding, base) =
+    match Sys.argv with
+      | [| _ |] -> (50000, 15, 10000, 9000)
+      | [| _; c; n; p; b |] -> (int_of_string c, int_of_string n, int_of_string p, int_of_string b)
+      | _ ->
+	(Printf.printf "Usage: t.native | t.native problem_size nrepeats padding base\n";
+	 raise Exit)
+  in
 
-  let str4s = range_fold (fun n a -> str4 n :: a) max_count [] in
-  let str4s_probe = range_fold (fun n a -> str4 n :: a) max_count [] in
-  let missing = str4 (max_count * 2) in
+  stats_header ["padding"; "base"];
+  let run_experiment label thunk =
+    stats nrepeats max_count [string_of_int padding; string_of_int base] label thunk
+  in
+
+  let str4s = range_fold (fun n a -> str4 padding base n :: a) max_count [] in
+  let str4s_probe = range_fold (fun n a -> str4 padding base n :: a) max_count [] in
+  let missing = str4 padding base (max_count * 2) in
 
   let str4s_fold_gen ss f seed =
     let rec loop xs seed = match xs with
@@ -70,33 +127,33 @@ let main () =
   let str4s_fold f seed = str4s_fold_gen str4s f seed in
   let str4s_probe_fold f seed = str4s_fold_gen str4s_probe f seed in
 
-  mean_of nrepeats max_count "Baseline"
+  run_experiment "Baseline"
     (fun () -> str4s_fold (fun n a -> let _ = n in a) ());
 
 
-  let full_c = mean_of nrepeats max_count "Critbit   - insertion"
+  let full_c = run_experiment "Critbit insertion"
     (fun () -> str4s_fold (fun n a -> add n a) empty) in
-  let full_s = mean_of nrepeats max_count "StringSet - insertion"
+  let full_s = run_experiment "StringSet insertion"
     (fun () -> str4s_fold (fun n a -> StringSet.add n a) StringSet.empty) in
 
-  let _ = mean_of nrepeats max_count "Critbit   - removal"
+  let _ = run_experiment "Critbit removal"
     (fun () -> str4s_probe_fold (fun n a -> remove n a) full_c) in
-  let _ = mean_of nrepeats max_count "StringSet - removal"
+  let _ = run_experiment "StringSet removal"
     (fun () -> str4s_probe_fold (fun n a -> StringSet.remove n a) full_s) in
 
-  mean_of nrepeats max_count "Critbit   - positive membership"
+  run_experiment "Critbit positive membership"
     (fun () -> str4s_probe_fold (fun n a -> let _ = mem n full_c in a) ());
-  mean_of nrepeats max_count "StringSet - positive membership"
+  run_experiment "StringSet positive membership"
     (fun () -> str4s_probe_fold (fun n a -> let _ = StringSet.mem n full_s in a) ());
 
-  mean_of nrepeats max_count "Critbit   - negative membership"
+  run_experiment "Critbit negative membership"
     (fun () -> str4s_probe_fold (fun n a -> let _ = mem missing full_c in a) ());
-  mean_of nrepeats max_count "StringSet - negative membership"
+  run_experiment "StringSet negative membership"
     (fun () -> str4s_probe_fold (fun n a -> let _ = StringSet.mem missing full_s in a) ());
 
-  mean_of nrepeats max_count "Critbit   - cardinality"
-    (fun () -> str4s_probe_fold (fun n a -> let _ = cardinal full_c in a) ());
-  (* mean_of nrepeats max_count "StringSet - cardinality" *)
+  (* run_experiment "Critbit cardinality" *)
+  (*   (fun () -> str4s_probe_fold (fun n a -> let _ = cardinal full_c in a) ()); *)
+  (* run_experiment "StringSet cardinality" *)
   (*   (fun () -> str4s_probe_fold (fun n a -> let _ = StringSet.cardinal full_s in a) ()); *)
 
   ();;
